3e40857c60f28596569e6229257d37d715cc243a
hudahamid <huda.00091581@gmail.com>
2025-01-23 12:33:06 +0300

    ..

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+bool icecream=false, pizza=false, salad=false, burger=false;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +24,7 @@ class _HomeState extends State<Home> {
           Row(
             mainAxisAlignment:MainAxisAlignment.spaceBetween ,
             children: [
-              Text("Hello me ",
+              Text("Hello Sara  ",
               style: AppWidget.boldTextFieldStyle()
              ),
              Container(
@@ -31,12 +35,118 @@ class _HomeState extends State<Home> {
             ],
           ),
           SizedBox(height: 30.0,),
-        Text("Delicious food ",
-              style: AppWidget.boldTextFieldStyle()
+           Text("Delicious food ",
+              style: AppWidget.headerTextFieldStyle()
              ),
-          
+            Text("Discover and  Get Great Food",
+              style: AppWidget.LightTextFieldStyle()
+             ),
+             SizedBox(height: 20.0,),
+             showItem(),
+             SizedBox(height:  20.0,),
+             Row(children: [
+              Container(
+                child: Column(children: [
+                  Image.asset("images/salad2.png", height: 150,width: 150, fit: BoxFit.cover,),
+                  Text("Salad", style: AppWidget.SemiBoldTextFieldStyle(),),
+                  Text("Fresh and Healthy", style: AppWidget.LightTextFieldStyle(),),
+                ],),
+              )
+             ],),
+         
       ],) 
       ,)
     );
+  }
+
+
+  Widget showItem(){
+  return  Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+             GestureDetector(
+              onTap: (){
+                pizza=false;
+                icecream=true;
+                salad=false;
+                burger=false;
+                setState(() {
+                  
+                });
+              },
+              child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  decoration: BoxDecoration(color: icecream?Colors.black : Colors.white,borderRadius: BorderRadius.circular(10)),
+                  padding: EdgeInsets.all(8),
+                  child: Image.asset("images/ice-cream.png",height: 50,width: 50, fit: BoxFit.cover,color: icecream?Colors.white:Colors.black,),
+                ) ,
+              ),
+            ),
+
+              GestureDetector(
+              onTap: (){
+                pizza=false;
+                icecream=false;
+                salad=true;
+                burger=false;
+                setState(() {
+                  
+                });
+              },
+               child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                   decoration: BoxDecoration(color: salad?Colors.black : Colors.white,borderRadius: BorderRadius.circular(10)),
+                  padding: EdgeInsets.all(8),
+                  child: Image.asset("images/salad.png",height: 50,width: 50, fit: BoxFit.cover,color: salad?Colors.white:Colors.black,),
+                ) ,
+                           ),
+             ),
+
+             GestureDetector(
+              onTap: (){
+                pizza=false;
+                icecream=false;
+                salad=false;
+                burger=true;
+                setState(() {
+                  
+                });
+              },
+               child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                   decoration: BoxDecoration(color: burger?Colors.black : Colors.white,borderRadius: BorderRadius.circular(10)),
+                  padding: EdgeInsets.all(8),
+                  child: Image.asset("images/burger.png",height: 50,width: 50, fit: BoxFit.cover,color: burger?Colors.white:Colors.black,),
+                ) ,
+                           ),
+             ),
+
+              GestureDetector(
+              onTap: (){
+                pizza=true;
+                icecream=false;
+                salad=false;
+                burger=false;
+                setState(() {
+                  
+                });
+              },
+               child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                   decoration: BoxDecoration(color: pizza?Colors.black : Colors.white,borderRadius: BorderRadius.circular(10)),
+                  padding: EdgeInsets.all(8),
+                  child: Image.asset("images/pizza.png",height: 50,width: 50, fit: BoxFit.cover,color: pizza?Colors.white:Colors.black,),
+                ) ,
+                           ),
+             ),
+          ],);
   }
 }
--- lib/widget/widget_support.dart
+++ lib/widget/widget_support.dart
@@ -9,4 +9,24 @@ static TextStyle boldTextFieldStyle(){
 }
 
 
+
+static TextStyle headerTextFieldStyle(){
+  return  TextStyle(
+                color: Colors.black,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold);
+}
+static TextStyle LightTextFieldStyle(){
+  return  TextStyle(
+                color: Colors.black38,
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold);
+}
+
+static TextStyle SemiBoldTextFieldStyle(){
+  return  TextStyle(
+                color: Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w500);
+}
 }
